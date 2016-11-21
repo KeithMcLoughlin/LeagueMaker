322082df73b4a2cd26758747801b37b20794767a
keithmcloughlin <c14337491@mydit.ie>
2016-11-21 21:07:22 +0000

    changed add league sport field to a spinner

--- LeagueMaker/app/src/main/java/com/example/keith/leaguemaker/AddLeagueActivity.java
+++ LeagueMaker/app/src/main/java/com/example/keith/leaguemaker/AddLeagueActivity.java
@@ -2,12 +2,35 @@ package com.example.keith.leaguemaker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class AddLeagueActivity extends AppCompatActivity {
+public class AddLeagueActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    String[] items = {"Football", "Ice Hockey", "Basketball"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_league);
+
+        Spinner sports = (Spinner)findViewById(R.id.pickSport);
+        sports.setOnItemSelectedListener(this);
+
+        ArrayAdapter<String> spinData = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
+        spinData.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sports.setAdapter(spinData);
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id)
+    {
+
+    }
+
+    public void onNothingSelected(AdapterView<?> parent)
+    {
+
     }
 }
