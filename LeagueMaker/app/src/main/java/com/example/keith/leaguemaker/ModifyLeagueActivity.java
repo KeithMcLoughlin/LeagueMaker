9b4ce71318b9d6d3f0ab5c4f818ed82b23065d94
keithmcloughlin <c14337491@mydit.ie>
2016-11-24 22:55:24 +0000

    added modify league that carries over the league details

--- LeagueMaker/app/src/main/java/com/example/keith/leaguemaker/ModifyLeagueActivity.java
+++ LeagueMaker/app/src/main/java/com/example/keith/leaguemaker/ModifyLeagueActivity.java
@@ -1,13 +1,60 @@
 package com.example.keith.leaguemaker;
 
+import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-public class ModifyLeagueActivity extends AppCompatActivity {
+public class ModifyLeagueActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    String[] items = {"Football", "Ice Hockey", "Basketball"};
+    Cursor leagueToBeModified;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify_league);
+
+        Spinner sports = (Spinner)findViewById(R.id.pickSport);
+        sports.setOnItemSelectedListener(this);
+
+        ArrayAdapter<String> spinData = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
+        spinData.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sports.setAdapter(spinData);
+
+        Bundle passedRow = getIntent().getExtras();
+        DBManager dbm = new DBManager(this);
+        dbm.open();
+        String query = "Select * from League where _id = " + passedRow.getInt("leagueRow");
+        leagueToBeModified = dbm.rawQuery("Select * from League where _id = " + passedRow.getInt("leagueRow"));
+        leagueToBeModified.moveToFirst();
+        //Log.d("hello2", leagueToBeModified.getString(leagueToBeModified.getColumnIndex("leagueName")));
+
+        dbm.close();
+
+        String modifyName = leagueToBeModified.getString(leagueToBeModified.getColumnIndex("leagueName"));
+        String modifySport = leagueToBeModified.getString(leagueToBeModified.getColumnIndex("sport"));
+
+        EditText leagueName = (EditText)findViewById(R.id.editname);
+        leagueName.setText(modifyName);
+        int pos = spinData.getPosition(modifySport);
+        sports.setSelection(pos);
+
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id)
+    {
+
+    }
+
+    public void onNothingSelected(AdapterView<?> parent)
+    {
+
     }
 }
--- LeagueMaker/app/src/main/java/com/example/keith/leaguemaker/ViewLeaguesActivity.java
+++ LeagueMaker/app/src/main/java/com/example/keith/leaguemaker/ViewLeaguesActivity.java
@@ -1,6 +1,7 @@
 package com.example.keith.leaguemaker;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.DatabaseUtils;
 import android.support.v7.app.AppCompatActivity;
@@ -15,14 +16,15 @@ import android.widget.Spinner;
 
 import java.util.Arrays;
 
-public class ViewLeaguesActivity extends ListActivity implements AdapterView.OnItemSelectedListener {
+public class ViewLeaguesActivity extends ListActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
 
+    Spinner leagues;
     Cursor allLeagues;
     Cursor allTeams;
     String[] columns;
     int[] to;
     SimpleCursorAdapter teamsCA;
-    String[] leagueNames;
+    View modifyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,19 +36,11 @@ public class ViewLeaguesActivity extends ListActivity implements AdapterView.OnI
         allLeagues = dbm.getAllLeagues();
         allTeams = dbm.getAllTeams();
 
-        leagueNames = new String[allLeagues.getCount()];
-        int i = 0;
-        while(allLeagues.moveToNext()){
-            String name = allLeagues.getString(allLeagues.getColumnIndex("leagueName"));
-            leagueNames[i] = name;
-            i++;
-        }
-
-        Spinner leagues = (Spinner)findViewById(R.id.pickLeague);
+        leagues = (Spinner) findViewById(R.id.pickLeague);
         leagues.setOnItemSelectedListener(this);
 
-        String[] spinCol = new String[] { "leagueName" };
-        int[] spinTo = new int[] { android.R.id.text1 };
+        String[] spinCol = new String[]{"leagueName"};
+        int[] spinTo = new int[]{android.R.id.text1};
 
         SimpleCursorAdapter spinData = new SimpleCursorAdapter(this, android.R.layout.simple_spinner_item, allLeagues, spinCol, spinTo);
         spinData.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -57,21 +51,32 @@ public class ViewLeaguesActivity extends ListActivity implements AdapterView.OnI
         teamsCA = new SimpleCursorAdapter(ViewLeaguesActivity.this, R.layout.view_leagues_row, allTeams, columns, to, 0);
         setListAdapter(teamsCA);
 
+        modifyButton = findViewById(R.id.modifyButton);
+        modifyButton.setOnClickListener(this);
+
         dbm.close();
     }
 
-    public void onItemSelected(AdapterView<?> parent, View v, int position, long id)
-    {
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
 
     }
 
-    public void onNothingSelected(AdapterView<?> parent)
-    {
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 
-    protected void onListItemClick(ListView l, View v, int position, long id)
-    {
+    protected void onListItemClick(ListView l, View v, int position, long id) {
 
+    }
+
+    public void onClick(View v) {
+        if (v.getId() == modifyButton.getId()) {
+            int position = leagues.getSelectedItemPosition() + 1;
+
+            Intent changeToModify = new Intent(this, ModifyLeagueActivity.class);
+            changeToModify.putExtra("leagueRow", position);
+            startActivity(changeToModify);
+
+        }
     }
 }
